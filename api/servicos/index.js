717e0e138ea2b84c2d7441a08e7df150efe538b0
Matheus Henrique <matheushenriquefs07@gmail.com>
2026-06-23 06:45:37 -0300

    fix: remove roteamento desnecessário e insere serviços

---
+++ api/servicos/index.js
@@ -0,0 +1,20 @@
+import { getSql } from '../_lib/db.js';
+import { sendJson, sendError, methodNotAllowed } from '../_lib/response.js';
+
+export default async function handler(req, res) {
+  if (req.method !== 'GET') return methodNotAllowed(res);
+
+  try {
+    const sql = getSql();
+    const servicos = await sql`
+      SELECT id, nome, descricao, preco, tempo_estimado
+      FROM servicos
+      WHERE ativo = TRUE
+      ORDER BY id
+    `;
+
+    return sendJson(res, 200, { servicos });
+  } catch {
+    return sendError(res, 500, 'Não foi possível listar os serviços.');
+  }
+}
